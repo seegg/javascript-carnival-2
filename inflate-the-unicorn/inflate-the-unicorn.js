045b59f4774fc2172d480cfc29d66f9c27e7689b
yuan tan <dyxtan@gmail.com>
2021-03-04 14:13:08 +1300

    implement inflate function

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -5,13 +5,27 @@
 console.log("Inflate The Unicorn!")
 
 
-function inflate(){
+function inflate(elem){
+
   let unicorn = [
     './images/unicorn-0.png',
     './images/unicorn-1.png',
     './images/unicorn-2.png',
     './images/unicorn-3.png'
-  ]
-
-  console.log('inflate');
+  ];
+  
+  for(let i = 0; i < unicorn.length; i++){
+    if(elem.getAttribute('src') === unicorn[i]){
+      if(i >= unicorn.length -1){
+        alert(elem.id.replace('-', ' ') + ' says thank you!');
+        elem.setAttribute('src', unicorn[0]);
+        console.log('deflate');
+        break;
+      }else{
+        console.log('inflate');
+        elem.setAttribute('src', unicorn[i+1]);
+        break;
+      }
+    }
+  }
 }
