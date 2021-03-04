51113df3e554d95813cbefad909ed2cca22bde9e
yuan tan <dyxtan@gmail.com>
2021-03-04 13:49:25 +1300

    add inflate function

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -3,3 +3,15 @@
 // -    -   -   -   -  //
 
 console.log("Inflate The Unicorn!")
+
+
+function inflate(){
+  let unicorn = [
+    './images/unicorn-0.png',
+    './images/unicorn-1.png',
+    './images/unicorn-2.png',
+    './images/unicorn-3.png'
+  ]
+
+  console.log('inflate');
+}
